9585fae9ca82b5828cee4d576095c372f3950a3f
Vincent <vincent00lav@outlook.com>
2025-03-03 15:17:44 +0100

    Header Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 function Footer() {
-    return (
-        <p>Copyright Mexican Food 2024</p>
-    )
+  return (
+    <footer>
+      <p>Copyright Mexican Food 2024</p>
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 function Header() {
   return (
-    <>
+    <header>
       <img
         src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-        alt="logo"
+        alt="Un mexicain qui joue de la guitare"
       />
       <nav>
         <a href="/">Accueil</a>
       </nav>
-    </>
+    </header>
   );
 }
 export default Header;
