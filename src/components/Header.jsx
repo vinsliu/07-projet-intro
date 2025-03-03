e3942e21031d505ebe3f9481a617a988a46731c4
Vincent <vincent00lav@outlook.com>
2025-03-03 13:57:42 +0100

    Activite 4 : Composant Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <img
+        src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+        alt="logo"
+      />
+      <a href="/">Accueil</a>
+    </>
+  );
+}
+export default Header;
