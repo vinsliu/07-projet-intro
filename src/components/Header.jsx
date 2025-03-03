1e27837d76ccd0118f0317302a7f8be4b76967eb
Vincent <vincent00lav@outlook.com>
2025-03-03 14:06:59 +0100

    Header nav ajoute

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ function Header() {
         src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
         alt="logo"
       />
-      <a href="/">Accueil</a>
+      <nav>
+        <a href="/">Accueil</a>
+      </nav>
     </>
   );
 }
