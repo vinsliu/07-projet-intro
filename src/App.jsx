9de4eed84af22c5da12d1ae92b1c2094225ce59f
Vincent <vincent00lav@outlook.com>
2025-03-03 13:59:45 +0100

    Activite 5 : Composant Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Header from './components/Header'
-import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer() {
+    return (
+        <p>Copyright Mexican Food 2024</p>
+    )
+}
+export default Footer
