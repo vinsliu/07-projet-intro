066a8405771ddb06eca96603ba0e1a6156370141
Vincent <vincent00lav@outlook.com>
2025-03-04 16:34:28 +0100

    Activite 10 finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,44 @@ import "./App.scss";
 import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
+  const dishes = [
+    {
+      name: "Tacos à l’unité",
+      image:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      price: "3€",
+      isNew: true,
+    },
+    {
+      name: "Enchiladas",
+      image:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      price: "12€",
+    },
+    {
+      name: "Mole poblano",
+      image:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      price: "15€",
+    },
+  ];
+
   return (
     <>
       <Header />
       <main>
         <Container>
           <Row>
-            <Col md={4}>
-              <Dish
-                image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                name="Tacos à l’unité"
-                price="3€"
-                isNew={true}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                name="Enchiladas"
-                price="12€"
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                name="Mole poblano"
-                price="15€"
-              />
-            </Col>
+            {dishes.map((dish) => (
+              <Col md={4}>
+                <Dish
+                  image={dish.image}
+                  name={dish.name}
+                  price={dish.price}
+                  isNew={dish.isNew}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
