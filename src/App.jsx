ec3ce8abff6b7cb4046f571c4a65c55dfc1ce98f
Vincent <vincent00lav@outlook.com>
2025-03-04 16:49:57 +0100

    Activite 10 updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       name: "Tacos à l’unité",
       image:
         "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
-      price: "3",
+      price: 3,
       isNew: true,
     },
     {
@@ -19,14 +19,14 @@ function App() {
       name: "Enchiladas",
       image:
         "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
-      price: "12",
+      price: 12,
     },
     {
       id: 3,
       name: "Mole poblano",
       image:
         "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
-      price: "15",
+      price: 15,
     },
   ];
 
@@ -37,9 +37,8 @@ function App() {
         <Container>
           <Row>
             {dishes.map((dish) => (
-              <Col md={4}>
+              <Col md={4} key={dish.id}>
                 <Dish
-                  key={dish.id}
                   image={dish.image}
                   name={dish.name}
                   price={dish.price}
