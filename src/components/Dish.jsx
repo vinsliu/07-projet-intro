19224be7b7a843019da147882416cac6802ffd93
Vincent <vincent00lav@outlook.com>
2025-03-04 15:15:47 +0100

    Activite 9 updated

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -2,21 +2,10 @@ import { Card, Badge } from "react-bootstrap";
 import "../assets/scss/dish.scss";
 
 function Dish({ image, name, price, isNew = false }) {
-  if (isNew) {
-    return (
-      <Card className="Card">
-        <Card.Img variant="top" src={image} alt={name} />
-        <Badge>Nouveau</Badge>
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{price}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  }
   return (
     <Card className="Card">
       <Card.Img variant="top" src={image} alt={name} />
+      {isNew === true ? <Badge>Nouveau</Badge> : ""}
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>{price}</Card.Text>
@@ -24,4 +13,5 @@ function Dish({ image, name, price, isNew = false }) {
     </Card>
   );
 }
+
 export default Dish;
