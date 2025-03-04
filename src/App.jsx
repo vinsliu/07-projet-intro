b7ad2ecd5d5ac1a1bef52294614ebc4cca08153c
Vincent <vincent00lav@outlook.com>
2025-03-04 16:55:19 +0100

    Activite 11 updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       price: 3,
       isNew: true,
-      active: true,
+      active: 12,
     },
     {
       id: 2,
@@ -21,7 +21,7 @@ function App() {
       image:
         "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       price: 12,
-      active: true,
+      active: 0,
     },
     {
       id: 3,
@@ -29,7 +29,7 @@ function App() {
       image:
         "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
       price: 15,
-      active: true,
+      active: 5,
     },
   ];
 
